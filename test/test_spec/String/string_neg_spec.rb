d4d53930bcadbf96ee4424bacfb78d8bcca85ea4
dsk04 <dishantkalyani.com>
2016-09-08 23:15:57 -0700

    test app

---
+++ test/test_spec/String/string_neg_spec.rb
@@ -0,0 +1,12 @@
+require_relative '../../../lib/tasks/string_analyzer'
+
+describe StringAnalyzer do
+
+  it "shuld detect when any string does not contain vowel" do
+    s = StringAnalyzer.new
+    test_string = 'bnvmxcz'
+    expect(s.has_vowels(test_string)).to be false
+
+  end
+
+end
---
+++ test/test_spec/StringAnalyzer/string_analyze_spec.rb
@@ -0,0 +1,18 @@
+
+require_relative '../../../lib/tasks/string_analyzer'
+
+  describe StringAnalyzer do
+
+    it "should detect when any string contains vowel" do
+      s = StringAnalyzer.new
+      test_string ='dddeee'
+      expect(s.has_vowels(test_string)).to be true
+    end
+
+
+  end
+
+
+
+
+
